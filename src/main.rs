8ebdf894c8b17c28b8cc808c06ac05aa38575332
Pedro Lucas <plrf100@gmail.com>
2021-09-21 21:14:52 -0300

    git(init): initializing repo

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate image;
+
+
+fn convert_bit_to_u8(value: f32) -> u8 {
+  let max = 255.0;
+  (max * value) as u8
+}
+
+fn main() {
+    let im_height = 256;
+    let im_width = 256;
+
+    let mut imgbuf = image::ImageBuffer::new(im_width, im_height);
+
+    for (i, j, pixel) in imgbuf.enumerate_pixels_mut() {
+      // let r = (0.3 * x as f32) as u8;
+      let r = ((i as f32 - 1.0) / (im_width as f32 - 1.0)) as f32;
+      let g = (1.0 - (j as f32 - 1.0) / (im_height as f32 - 1.0)) as f32;
+      let b = 0.25 as f32;
+      
+      let r = convert_bit_to_u8(r);
+      let g = convert_bit_to_u8(g);
+      let b = convert_bit_to_u8(b);
+
+      *pixel = image::Rgb([r, g, b]);
+  }
+    
+    imgbuf.save("src/rendered/image0.png").unwrap();
+    println!("Hello, world!");
+}
