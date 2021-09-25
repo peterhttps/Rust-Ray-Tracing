29d576de0493e82da7738ff94fcc8fda65aaf3c1
peterhttps <plrf100@gmail.com>
2021-09-25 00:52:18 -0300

    refact(code): optmizing code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod vector;
 
 extern crate image;
 
+use indicatif::ProgressBar;
 use rand::Rng;
 use std::{f32, time::Instant};
 use nalgebra::{ArrayStorage, Matrix, U1, U3, Vector3};
@@ -52,7 +53,7 @@ fn main() {
     // IMAGE
     let aspect_ratio: f32 = 16.0 / 9.0;
     let im_width = 800.0;
-    let im_height = (im_width / aspect_ratio) as u32;
+    let im_height: u32 = (im_width / aspect_ratio) as u32;
     let mut imgbuf = image::ImageBuffer::new(im_width as u32, im_height);
 
     // CAMERA
@@ -75,32 +76,41 @@ fn main() {
     // world.push(s4);
     world.push(floor);
 
+    println!("\nRendering time...");
+
     let samples_per_pixel = 100;
     let mut rng = rand::thread_rng();
 
-
+    let bar = ProgressBar::new(im_height as u64);
     let start = Instant::now();
-    for (i, j, pixel) in imgbuf.enumerate_pixels_mut() {
-      let mut pixelcolor = Vector3::new(0.0, 0.0, 0.0);
-      for n in 0..samples_per_pixel {
-        let u = (i as f32 - 1.0 + rng.gen::<f32>()) / (im_width as f32 - 1.0) as f32; 
-        let v = (1.0 - (j as f32 - 1.0 + rng.gen::<f32>()) / (im_height as f32 - 1.0)) as f32;
-        let dir = lower_left_corner + u*horizontal + v*vertical - origin;
-        let ray = Ray::new(origin, dir);
-  
-        pixelcolor += ray_color(ray, world.clone());
-      }
 
-      pixelcolor = pixelcolor / samples_per_pixel as f32;
-
-      let r = convert_bit_to_u8(pixelcolor.x);
-      let g = convert_bit_to_u8(pixelcolor.y);
-      let b = convert_bit_to_u8(pixelcolor.z);
-      *pixel = image::Rgb([r, g, b]);
-    }
+    for j in 0..im_height as u32 {
+      for i in 0..im_width as u32 {
+        let mut pixelcolor = Vector3::new(0.0, 0.0, 0.0);
+        for n in 0..samples_per_pixel {
+          let u = (i as f32 - 1.0 + rng.gen::<f32>()) / (im_width as f32 - 1.0) as f32; 
+          let v = (1.0 - (j as f32 - 1.0 + rng.gen::<f32>()) / (im_height as f32 - 1.0)) as f32;
+          let dir = lower_left_corner + u*horizontal + v*vertical - origin;
+          let ray = Ray::new(origin, dir);
     
+          pixelcolor += ray_color(ray, world.clone());
+        }
+
+        pixelcolor = pixelcolor / samples_per_pixel as f32;
+
+        let pixel = imgbuf.get_pixel_mut(i, j);
+
+        let r = convert_bit_to_u8(pixelcolor.x);
+        let g = convert_bit_to_u8(pixelcolor.y);
+        let b = convert_bit_to_u8(pixelcolor.z);
+        *pixel = image::Rgb([r, g, b]);
+      }
+      bar.inc(1);
+    }
+
     imgbuf.save("src/rendered/image7.png").unwrap();
     let duration = start.elapsed();
-    println!("Time elapsed: {:?}", duration);
+    println!("Render done!");
+    println!("Time elapsed: {:.2?}", duration);
     println!("Render done!");
 }
